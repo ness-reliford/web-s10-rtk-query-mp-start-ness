98fba0d40e722887d0a6621ab6ecc3f5db8e8a0c
Ness Reliford <nessreliford@gmail.com>
2024-10-16 12:04:07 -0700

    all tasks completed

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,47 @@
 // create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const quotesApi = createApi({
+
+    reducerPath: 'quotesApi',
+    baseQuery: fetchBaseQuery({baseUrl:"http://localhost:9009/api"}),
+    tagTypes: ['quotes'],
+    endpoints: builder => ({
+        getQuotes: builder.query({
+            query: () => 'quotes',
+            providesTags: ['quotes'],
+        }),
+        createQuote: builder.mutation({
+            query: quote => ({
+                url: 'quotes',
+                method: 'POST',
+                body: quote
+            }),
+            invalidatesTags: ['quotes']
+        }),
+        toggleFake: builder.mutation({
+            query: ({id, quote}) => ({
+                url: `quotes/${id}`,
+                method: 'PUT',
+                body: quote
+            }),
+            invalidatesTags: ['quotes']
+        }),
+        deleteQuote: builder.mutation({
+            query: id => ({
+                url: `quotes/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['quotes']
+        })
+    })
+
+
+})
+
+export const {
+    useGetQuotesQuery,
+    useToggleFakeMutation, 
+    useCreateQuoteMutation, 
+    useDeleteQuoteMutation,
+} = quotesApi
